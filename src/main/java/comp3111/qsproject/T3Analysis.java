a74fb3b33664ee497eca6047117d4f9255dfdf0e
Ipsitacodes <issingh@connect.ust.hk>
2024-04-20 17:28:40 +0800

    Added the "All" functionality in T3Analysis.class constructor and fixed bugs related to string comparison

--- src/main/java/comp3111/qsproject/T3Analysis.java
+++ src/main/java/comp3111/qsproject/T3Analysis.java
@@ -21,36 +21,59 @@ public class T3Analysis {
         // Iterate over the QSList
         int bottom = Integer.parseInt(bottom_input);
         int top = Integer.parseInt(top_input);
+        System.out.println(QSList.list.size());
+
         for (QSItem item : QSList.list) {
             // Convert the item's score to an integer
             int itemScore = Integer.parseInt(item.getRank());
+            if (!region.equals("All")) {
+                // Check if the item fits the score range, type, and region
+                if (itemScore <= bottom && itemScore >= top && item.getType().equals(type) && item.getRegion().equals(region)) {
+                    // If a RecommendItem for this QSItem already exists in the RecommendList, update it
+                    boolean found = false;
+                    for (RecommendItem recommendItem : RecommendList) {
+                        if (recommendItem.getName().equals(item.name)) {
+                            recommendItem.update(item);
+                            found = true;
+                            break;
+                        }
+                    }
 
-            // Check if the item fits the score range, type, and region
-            if (itemScore >= bottom && itemScore <= top && item.type.equals(type) && item.region.equals(region)) {
-                // If a RecommendItem for this QSItem already exists in the RecommendList, update it
-                boolean found = false;
-                for (RecommendItem recommendItem : RecommendList) {
-                    if (recommendItem.name.equals(item.name)) {
-                        recommendItem.update(item);
-                        found = true;
-                        break;
+                    // If a RecommendItem for this QSItem does not exist in the RecommendList, create a new one
+                    if (!found) {
+                        RecommendList.add(new RecommendItem(item));
                     }
                 }
+            }
+            else{
+                if (itemScore <= bottom && itemScore >= top && item.getType().equals(type)) {
+                    // If a RecommendItem for this QSItem already exists in the RecommendList, update it
+                    boolean found = false;
+                    for (RecommendItem recommendItem : RecommendList) {
+                        if (recommendItem.getName().equals(item.name)) {
+                            recommendItem.update(item);
+                            found = true;
+                            break;
+                        }
+                    }
 
-                // If a RecommendItem for this QSItem does not exist in the RecommendList, create a new one
-                if (!found) {
-                    RecommendList.add(new RecommendItem(item));
+                    // If a RecommendItem for this QSItem does not exist in the RecommendList, create a new one
+                    if (!found) {
+                        RecommendList.add(new RecommendItem(item));
+                    }
                 }
             }
         }
 
         // Sort the RecommendList by bestRank
-        RecommendList.sort((item1, item2) -> Integer.compare(Integer.parseInt(item1.bestRank), Integer.parseInt(item2.bestRank)));
+        System.out.println(this.RecommendList.size());
+        RecommendList.sort((item1, item2) -> Integer.compare(Integer.parseInt(item1.getBestRank()), Integer.parseInt(item2.getBestRank())));
 
     }
 
     ObservableList<RecommendItem> getRecommendData() {
         // Show the most valuable university
+        System.out.println(this.RecommendList.size());
         return RecommendList;
     }
 }
