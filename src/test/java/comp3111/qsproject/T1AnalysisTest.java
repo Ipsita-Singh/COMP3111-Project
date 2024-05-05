a1e1986e12ce4e59fe2d483671ae95361da87ae4
Malav Daftary <anmoldaftary3004@gmail.com>
2024-05-05 15:37:22 +0800

    feat!: Completed tests for T1Analysis

--- src/test/java/comp3111/qsproject/T1AnalysisTest.java
+++ src/test/java/comp3111/qsproject/T1AnalysisTest.java
@@ -1,6 +1,8 @@
 package comp3111.qsproject;
 
+import javafx.scene.chart.BarChart;
 import javafx.scene.chart.PieChart;
+import javafx.scene.chart.XYChart;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import javafx.collections.FXCollections;
@@ -8,45 +10,47 @@ import javafx.collections.ObservableList;
 import java.util.*;
 import java.util.stream.Collectors;
 
+import static java.lang.Long.parseLong;
 import static org.junit.jupiter.api.Assertions.*;
 
 class T1AnalysisTest {
 
-        /*
     @Test
-    void DataTabletest() {
-        QSList.initialize();
-        String year = "2018";
-
-
-        T1Analysis tester = new T1Analysis(year);
-        //List <QSItem> expectedTable = new
-        //Assertions.assertEquals(tester.getTableList(),
-
-    }
-    */
-
-
-    @Test
-    void PieChartDatatest() {
+    void PieChartDatatestSize() {
         QSList.initialize();
         String year = "2019";
 
         T1Analysis tester = new T1Analysis(year);
         ObservableList<PieChart.Data> ExpectedpieChartData= FXCollections.observableArrayList();
 
-        /*
-        2019 size data:
-        XL: 5979
-        L: 9651
-        M: 2972
-        S: 887
-         */
-        ExpectedpieChartData.add(new PieChart.Data("L: 9651", 9651));
-        ExpectedpieChartData.add(new PieChart.Data("XL: 5979", 5979));
-        ExpectedpieChartData.add(new PieChart.Data("M: 2972", 2972));
-        ExpectedpieChartData.add(new PieChart.Data("S: 887", 887));
-        assertEquals(ExpectedpieChartData,tester.getPieChartData("size"));
+        ExpectedpieChartData.add(new PieChart.Data("L: 9651", parseLong("9651")));
+        ExpectedpieChartData.add(new PieChart.Data("XL: 5980", parseLong("5980")));
+        ExpectedpieChartData.add(new PieChart.Data("M: 2972", parseLong("2972")));
+        ExpectedpieChartData.add(new PieChart.Data("S: 888", parseLong("888")));
+
+        ObservableList<PieChart.Data> ActualData = tester.getPieChartData("size");
+        for (int i = 0; i < 4; i++){
+            Assertions.assertEquals(ExpectedpieChartData.get(i).getPieValue(),ActualData.get(i).getPieValue());
+        }
+    }
+
+    @Test
+    void BarChartDatatestSize() {
+        QSList.initialize();
+        String year = "2017";
+
+        T1Analysis tester = new T1Analysis(year);
+        XYChart.Series<String, Double> ExpectedBarChartData = new XYChart.Series<>();
+
+        ExpectedBarChartData.getData().add(new XYChart.Data<>("XL", 54.549137931034494));
+        ExpectedBarChartData.getData().add(new XYChart.Data<>("L", 51.407462686567186));
+        ExpectedBarChartData.getData().add(new XYChart.Data<>("S", 51.25294117647061));
+        ExpectedBarChartData.getData().add(new XYChart.Data<>("M", 46.89230769230767));
+
+        XYChart.Series<String, Double> ActualData = tester.getBarChartData("size");
+        for (int i = 0; i < 4; i++){
+            Assertions.assertEquals(ExpectedBarChartData.getData().get(i).getYValue(),ActualData.getData().get(i).getYValue());
+        }
     }
 
 }
